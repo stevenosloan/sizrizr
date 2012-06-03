61eaec49f1fef703f58b2dbe7d6c55a6781f21ca
Steven Sloan <stevenosloan@gmail.com>
2012-06-03 01:39:06 -0400

    add rudamentary test for defined widths

--- script/sizrizr.js
+++ script/sizrizr.js
@@ -1,16 +1,33 @@
 // Sizrizr
-window.Sizrizr = (function( window, document, undefined ) {
+window.Sizrizr = (function( window, document ) {
 
   var docElement = document.documentElement;
-
   var classes = [ "sizrizr" ];
 
 
+  // Get the window Width
+  var winWidth = 0;
+  if( typeof( window.innerWidth ) == 'number' ) {
+    //Non-IE
+    winWidth = window.innerWidth;
+  } else if( document.documentElement && ( document.documentElement.clientWidth ) ) {
+    //IE 6+ in 'standards compliant mode'
+    winWidth = document.documentElement.clientWidth;
+  } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+    //IE 4 compatible
+    winWidth = document.body.clientWidth;
+  }
 
-  // Remove "no-js" class from <html> element, if it exists:
-  docElement.className = docElement.className.replace(/(^|\s)no-js(\s|$)/, '$1$2') +
+  // Create Classes based on window Width
+  if( winWidth < 580 ){
+    classes.push("sizrizr-small");
+  } else if( winWidth >= 580 && winWidth <= 960 ){
+    classes.push("sizrizr-mid");
+  } else if( winWidth > 960 ){
+    classes.push("sizrizr-large");
+  }
 
-  // Add the new classes to the <html> element.
-  ' js ' + classes.join(' ');
+  // Remove "no-js" class from <html> element, if it exists & add the new classes to the <html> element.
+  docElement.className = docElement.className.replace(/(^|\s)no-js(\s|$)/, '$1$2') + ' ' + classes.join(' ');
 
-})(this, this.document);
+})( this, this.document );
