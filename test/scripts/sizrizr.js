71617a568051b6a4b0470f8c6d91bcaf8d0100a2
Steven Sloan <stevenosloan@gmail.com>
2012-06-03 20:03:52 -0400

    shaving size

--- test/scripts/sizrizr.js
+++ test/scripts/sizrizr.js
@@ -1,60 +1,39 @@
 // Sizrizr
 window.Sizrizr = (function( window, document ) {
 
-  var docElement = document.documentElement;
-  var Sizrizr = {};
-  var classes = [ "sizrizr" ];
+  var docElement = document.documentElement,
+  Sizrizr = {},
+  classes = [ "sizrizr" ];
 
 
   // Get the window Width
-  var winWidth = 0;
-  if( typeof( window.innerWidth ) == 'number' ) {
-    //Non-IE
-    winWidth = window.innerWidth;
-  } else if( document.documentElement && ( document.documentElement.clientWidth ) ) {
-    //IE 6+ in 'standards compliant mode'
-    winWidth = document.documentElement.clientWidth;
-  } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
-    //IE 4 compatible
-    winWidth = document.body.clientWidth;
-  }
+  // var winWidth = 0;
+  // if( typeof( window.innerWidth ) == 'number' ) {
+  //   //Non-IE
+  //   winWidth = window.innerWidth;
+  // } else if( document.documentElement && ( document.documentElement.clientWidth ) ) {
+  //   //IE 6+ in 'standards compliant mode'
+  //   winWidth = document.documentElement.clientWidth;
+  // } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+  //   //IE 4 compatible
+  //   winWidth = document.body.clientWidth;
+  // }
+  var winWidth = ( typeof( window.innerWidth ) == 'number' ) ? window.innerWidth : ( document.documentElement && ( document.documentElement.clientWidth ) ) ? document.documentElement.clientWidth : ( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) ? document.body.clientWidth : 0;
 
   // name = string
   // type = string : "over", "under", or "between"
   // point = number or array : array if type is "between"
   Sizrizr.addPoint = function( name, type, point ){
 
-    var test = false;
+    var test = ( type === "between" && winWidth >= point[0] && winWidth <= point[1] ) ? true : ( type === "under" && winWidth < point ) ? true : ( type === "over" && winWidth > point ) ? true : false;
 
-    if( type === "between" && winWidth > point[0] && winWidth < point[1] ){
-
-        test = true;
-
-    } else {
-
-      if( type === "under" && winWidth < point ){
-
-        test = true;
-
-      } else if( type === "over" && winWidth > point ){
-
-        test = true;
-      
-      }
-
-    }
-
-
-    if( test === true ){
-
-      docElement.className += ' sizrizr-' + name;
-
-    }
+    docElement.className += (test === true) ? ' sizrizr-'+name : docElement.className;
 
     Sizrizr[name] = function( test ){
       return test;
     };
     return Sizrizr;
+
   };
 
 
