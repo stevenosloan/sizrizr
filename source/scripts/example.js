11e198c56aecafebc853a3f1831ce16e55e7c8c7
Steven Sloan <stevenosloan@gmail.com>
2013-04-09 17:00:28 -0400

    add #onchange_to

--- source/scripts/example.js
+++ source/scripts/example.js
@@ -1,5 +1,8 @@
 Sizrizr.addPoint( "small", "under", 580 );
 Sizrizr.addPoint( "mid", "between", [ 580, 960 ]);
 Sizrizr.addPoint( "large", "over", 960 );
+Sizrizr.onchange_to("small", function(){
+  alert( "I'm small now");
+});
 
 Sizrizr.run();
